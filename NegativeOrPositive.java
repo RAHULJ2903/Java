d7d1f1fb25add3a691e9bc212d165fcd2a977d1c
RAHULJ2903 <90106579+RAHULJ2903@users.noreply.github.com>
2021-09-04 22:27:50 +0530

    Add files via upload

---
+++ NegativeOrPositive.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class NegativeOrPositive {
+
+	public static void main(String[] args) {
+		int i= 35;
+		if (i>0) { 
+		System.out.println("NUMBER IS POSITIVE");
+		}
+		else if (i==0) {
+		System.out.println("NUMBER IS NEITHER NEGATIVE NOR POSITIVE");
+		}
+		else {
+			System.out.println("NUMBER IS NEGATIVE");
+		}
+	
+	}
+	
+
+	}
---
+++ NegativeToPositive.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		int x = -40;
+		if (x<0);
+		int y= (-1*x);
+		System.out.println("THE NUMBER " +x+" IS CONVERTED TO " +y);
+
+	}
+
+}
